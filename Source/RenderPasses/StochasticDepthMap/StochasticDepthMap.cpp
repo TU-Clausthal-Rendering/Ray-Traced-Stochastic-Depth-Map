0d29ff0fe147c2247985ffa4368fc00a31edb469
Felix <felixbruell@live.de>
2023-04-26 17:25:08 +0200

    started adding stochastic depth map

---
+++ Source/RenderPasses/StochasticDepthMap/StochasticDepthMap.cpp
@@ -0,0 +1,312 @@
+/***************************************************************************
+ # Copyright (c) 2015-23, NVIDIA CORPORATION. All rights reserved.
+ #
+ # Redistribution and use in source and binary forms, with or without
+ # modification, are permitted provided that the following conditions
+ # are met:
+ #  * Redistributions of source code must retain the above copyright
+ #    notice, this list of conditions and the following disclaimer.
+ #  * Redistributions in binary form must reproduce the above copyright
+ #    notice, this list of conditions and the following disclaimer in the
+ #    documentation and/or other materials provided with the distribution.
+ #  * Neither the name of NVIDIA CORPORATION nor the names of its
+ #    contributors may be used to endorse or promote products derived
+ #    from this software without specific prior written permission.
+ #
+ # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS "AS IS" AND ANY
+ # EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ # PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ # CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ # EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ # PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ # PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ # OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **************************************************************************/
+#include "StochasticDepthMap.h"
+
+namespace
+{
+    const std::string kDepthIn = "depthMap";
+    const std::string ksDepth = "stochasticDepth";
+    const std::string kProgramFile = "RenderPasses/StochasticDepthMap/StochasticDepth.ps.slang";
+    const std::string kStencilFile = "RenderPasses/StochasticDepthMap/Stencil.ps.slang";
+    const std::string kSampleCount = "SampleCount";
+    const std::string kAlpha = "Alpha";
+    const std::string kCullMode = "CullMode";
+    const std::string kLinearize = "linearize";
+    const std::string kDepthFormat = "depthFormat";
+    const std::string kStencil = "stencilMask";
+
+    const Gui::DropdownList kCullModeList =
+    {
+        { (uint32_t)RasterizerState::CullMode::None, "None" },
+        { (uint32_t)RasterizerState::CullMode::Back, "Back" },
+        { (uint32_t)RasterizerState::CullMode::Front, "Front" },
+    };
+
+    const Gui::DropdownList kSampleCountList =
+    {
+        { (uint32_t)1, "1" },
+        { (uint32_t)2, "2" },
+        { (uint32_t)4, "4" },
+        { (uint32_t)8, "8" },
+        { (uint32_t)16, "16" },
+    };
+}
+
+// code from stochastic-depth ambient occlusion:
+
+// Based on Pascal's Triangle, 
+// from https://www.geeksforgeeks.org/binomial-coefficient-dp-9/
+static int Binomial(int n, int k)
+{
+    std::vector<int> C(k + 1);
+    C[0] = 1;  // nC0 is 1 
+
+    for (int i = 1; i <= n; i++)
+    {
+        // Compute next row of pascal triangle using 
+        // the previous row 
+        for (int j = std::min(i, k); j > 0; j--)
+            C[j] = C[j] + C[j - 1];
+    }
+    return C[k];
+}
+
+static uint8_t count_bits(uint32_t v)
+{
+    uint8_t bits = 0;
+    for (; v; ++bits) { v &= v - 1; }
+    return bits;
+}
+
+void generateStratifiedLookupTable(int n, std::vector<int>& indices, std::vector<uint32_t>& lookUpTable) {
+
+    uint32_t maxEntries = uint32_t(std::pow(2, n));
+    //std::vector<int> indices(n + 1);
+    //std::vector<uint32_t> lookUpTable(maxEntries);
+    indices.resize(n + 1);
+    lookUpTable.resize(maxEntries);
+
+    // Generate index list
+    indices[0] = 0;
+    for (int i = 1; i <= n; i++) {
+        indices[i] = Binomial(n, i - 1) + indices[i - 1];
+    }
+
+    // Generate lookup table
+    std::vector<int> currentIndices(indices);
+    lookUpTable[0] = 0;
+    for (uint32_t i = 1; i < maxEntries; i++) {
+        int popCount = count_bits(i);
+        int index = currentIndices[popCount];
+        lookUpTable[index] = i;
+        currentIndices[popCount]++;
+    }
+}
+
+extern "C" FALCOR_API_EXPORT void registerPlugin(Falcor::PluginRegistry& registry)
+{
+    registry.registerClass<RenderPass, StochasticDepthMap>();
+}
+
+StochasticDepthMap::StochasticDepthMap(std::shared_ptr<Device> pDevice) : RenderPass(std::move(pDevice))
+{
+    mpFbo = Fbo::create(mpDevice.get());
+
+    DepthStencilState::Desc dsdesc;
+    // set stencil test to pass when values > 0 are present (not equal to 0)
+    dsdesc.setStencilEnabled(true);
+    //dsdesc.setStencilWriteMask(0);
+    //dsdesc.setStencilReadMask(1);
+    dsdesc.setStencilOp(DepthStencilState::Face::FrontAndBack, DepthStencilState::StencilOp::Keep, DepthStencilState::StencilOp::Keep, DepthStencilState::StencilOp::Keep);
+    dsdesc.setStencilFunc(DepthStencilState::Face::FrontAndBack, DepthStencilState::Func::NotEqual);
+    dsdesc.setStencilRef(0);
+    mpStencilState = DepthStencilState::create(dsdesc);
+
+
+    mpStencilPass = FullScreenPass::create(mpDevice, kStencilFile);
+    // modify stencil to write always a 1 if the depth test passes (pixel was not discarded)
+    //dsdesc.setStencilWriteMask(0xff);
+    dsdesc.setStencilOp(DepthStencilState::Face::FrontAndBack, DepthStencilState::StencilOp::Keep, DepthStencilState::StencilOp::Keep, DepthStencilState::StencilOp::Replace);
+    dsdesc.setStencilFunc(DepthStencilState::Face::FrontAndBack, DepthStencilState::Func::Always);
+    dsdesc.setStencilRef(1);
+    // disable depth write
+    dsdesc.setDepthEnabled(false);
+    dsdesc.setDepthWriteMask(false);
+    mpStencilPass->getState()->setDepthStencilState(DepthStencilState::create(dsdesc));
+}
+
+StochasticDepthMap::SharedPtr StochasticDepthMap::create(std::shared_ptr<Device> pDevice, const Dictionary& dict)
+{
+    SharedPtr pPass = SharedPtr(new StochasticDepthMap(std::move(pDevice)));
+    for (const auto& [key, value] : dict)
+    {
+        if (key == kSampleCount) pPass->mSampleCount = value;
+        else if (key == kAlpha) pPass->mAlpha = value;
+        else if (key == kCullMode) pPass->mCullMode = value;
+        else if (key == kLinearize) pPass->mLinearizeDepth = value;
+        else if (key == kDepthFormat) pPass->mDepthFormat = value;
+        else logWarning("Unknown field '" + key + "' in a DepthPass dictionary");
+    }
+    return pPass;
+}
+
+Dictionary StochasticDepthMap::getScriptingDictionary()
+{
+    Dictionary d;
+    d[kSampleCount] = mSampleCount;
+    d[kAlpha] = mAlpha;
+    d[kCullMode] = mCullMode;
+    d[kLinearize] = mLinearizeDepth;
+    d[kDepthFormat] = mDepthFormat;
+    return d;
+}
+
+RenderPassReflection StochasticDepthMap::reflect(const CompileData& compileData)
+{
+    RenderPassReflection reflector;
+    reflector.addInput(kDepthIn, "non-linear (primary) depth map").bindFlags(ResourceBindFlags::ShaderResource);
+    reflector.addInput(kStencil, "(optional) stencil-mask").format(ResourceFormat::R8Uint).flags(RenderPassReflection::Field::Flags::Optional);
+    reflector.addOutput(ksDepth, "stochastic depths in [0,1]").bindFlags(ResourceBindFlags::AllDepthViews).format(mDepthFormat).texture2D(0, 0, mSampleCount);
+    return reflector;
+}
+
+void StochasticDepthMap::compile(RenderContext* pRenderContext, const CompileData& compileData)
+{
+    mpState.reset(); // reset defines etc.
+    // force reload of camera cbuffer
+    mLastZNear = 0.0f;
+    mLastZFar = 0.0f;
+
+    // always sample at pixel centers for our msaa resource
+    static std::array<Fbo::SamplePosition, 16> samplePos = {};
+    mpFbo->setSamplePositions(mSampleCount, 1, samplePos.data());
+
+    // generate data for stratified sampling
+    std::vector<int> indices;
+    std::vector<uint32_t> lookUpTable;
+    generateStratifiedLookupTable(mSampleCount, indices, lookUpTable);
+
+    mpStratifiedIndices = Buffer::createStructured(mpDevice.get(), sizeof(indices[0]), uint32_t(indices.size()), ResourceBindFlags::ShaderResource, Buffer::CpuAccess::None, indices.data(), false);
+    mpStratifiedLookUpBuffer = Buffer::createStructured(mpDevice.get(), sizeof(lookUpTable[0]), uint32_t(lookUpTable.size()), ResourceBindFlags::ShaderResource, Buffer::CpuAccess::None, lookUpTable.data(), false);
+}
+
+void StochasticDepthMap::execute(RenderContext* pRenderContext, const RenderData& renderData)
+{
+    if (!mpScene) return;
+
+    auto pDepthIn = renderData[kDepthIn]->asTexture();
+    auto psDepths = renderData[ksDepth]->asTexture();
+    Texture::SharedPtr pStencilMask;
+    if (renderData[kStencil])
+    {
+        pStencilMask = renderData[kStencil]->asTexture();
+        if (!isStencilFormat(mDepthFormat))
+        {
+            logWarning("StochasticDepthMap depth format must have stencil to enable writing to stencil");
+            pStencilMask.reset();
+        }
+    }
+
+    auto pCamera = mpScene->getCamera();
+
+    // clear
+    pRenderContext->clearDsv(psDepths->getDSV().get(), 1.0f, 0, true, pStencilMask != nullptr);
+    mpFbo->attachDepthStencilTarget(psDepths);
+
+    if(!mpState)
+    {
+        Program::Desc desc;
+        desc.addShaderModules(mpScene->getShaderModules());
+        desc.addShaderLibrary(kProgramFile).vsEntry("vsMain").psEntry("psMain");
+        desc.addTypeConformances(mpScene->getTypeConformances());
+        desc.setShaderModel("6_2");
+        auto defines = mpScene->getSceneDefines();
+        defines.add("NUM_SAMPLES", std::to_string(mSampleCount));
+        defines.add("ALPHA", std::to_string(mAlpha));
+        if (mLinearizeDepth) defines.add("LINEARIZE");
+        auto pProgram = GraphicsProgram::create(mpDevice, desc, defines);
+
+        mpState = GraphicsState::create(mpDevice);
+        mpState->setProgram(pProgram);
+        mpVars = GraphicsVars::create(mpDevice, pProgram->getReflector());
+    }
+
+    if (pStencilMask)
+    {
+        FALCOR_PROFILE(pRenderContext, "Stencil Copy");
+        mpStencilPass["mask"] = pStencilMask;
+        mpStencilPass->execute(pRenderContext, mpFbo);
+
+        mpState->setDepthStencilState(mpStencilState);
+    }
+    else mpState->setDepthStencilState(nullptr);
+
+    {
+        FALCOR_PROFILE(pRenderContext, "Stochastic Depths");
+        // rasterize non-linear depths
+        mpState->setFbo(mpFbo);
+        auto var = mpVars->getRootVar();
+        var["stratifiedIndices"] = mpStratifiedIndices;
+        var["stratifiedLookUpTable"] = mpStratifiedLookUpBuffer;
+        var["depthBuffer"] = pDepthIn;
+
+        // set camera data
+        float zNear = pCamera->getNearPlane();
+        float zFar = pCamera->getFarPlane();
+        if (mLastZNear != zNear || mLastZFar != zFar)
+        {
+            var["CameraCB"]["zNear"] = zNear;
+            var["CameraCB"]["zFar"] = zFar;
+            mLastZNear = zNear;
+            mLastZFar = zFar;
+        }
+
+        mpScene->rasterize(pRenderContext, mpState.get(), mpVars.get(), mCullMode);
+    }
+}
+
+void StochasticDepthMap::renderUI(Gui::Widgets& widget)
+{
+    static const Gui::DropdownList kDepthFormats =
+    {
+        { (uint32_t)ResourceFormat::D16Unorm, "D16Unorm"},
+        { (uint32_t)ResourceFormat::D32Float, "D32Float" },
+        { (uint32_t)ResourceFormat::D24UnormS8, "D24UnormS8" },
+        //{ (uint32_t)ResourceFormat::D32FloatS8X24, "D32FloatS8X24" },
+    };
+
+    uint32_t depthFormat = (uint32_t)mDepthFormat;
+    if (widget.dropdown("Buffer Format", kDepthFormats, depthFormat))
+    {
+        mDepthFormat = ResourceFormat(depthFormat);
+        requestRecompile();
+    }
+
+    uint32_t cullMode = (uint32_t)mCullMode;
+    if (widget.dropdown("Cull mode", kCullModeList, cullMode))
+        mCullMode = (RasterizerState::CullMode)cullMode;
+
+    if (widget.dropdown("Sample Count", kSampleCountList, mSampleCount))
+        requestRecompile(); // reload pass (recreate texture)
+
+    if (widget.var("Alpha", mAlpha, 0.0f, 1.0f, 0.01f))
+        requestRecompile();
+
+    if (widget.checkbox("Linearize Depths", mLinearizeDepth))
+        requestRecompile();
+}
+
+void StochasticDepthMap::setScene(RenderContext* pRenderContext, const Scene::SharedPtr& pScene)
+{
+    mpScene = pScene;
+    mpState.reset();
+
+    // force reload of camera cbuffer
+    mLastZNear = 0.0f;
+    mLastZFar = 0.0f;
+}
