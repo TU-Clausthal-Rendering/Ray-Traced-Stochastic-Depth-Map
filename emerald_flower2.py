c1c1f803898e4a92b1cdaa255a4cc92b94cc14fc
kopaka <felixbruelllive.de>
2024-06-12 12:19:23 +0200

    removed some temporary scripts

--- emerald_flower2.py
+++
@@ -1,6 +0,0 @@
-
-from falcor import *
-
-m.scene.camera.position = float3(-30.008846, 1.870318, 44.177036)
-m.scene.camera.target = float3(-30.162111, 1.220261, 44.921307)
-m.scene.camera.up = float3(0.000142, 1.000000, -0.000689)
